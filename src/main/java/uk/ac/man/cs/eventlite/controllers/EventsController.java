272bc86dc18eb2465e945573433f890a76d7235b
Germaine George <germaine.george@student.manchester.ac.uk>
2022-03-04 11:30:28 +0000

    Create form and add controller method for saving new events

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,18 +1,26 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
 
 @Controller
@@ -21,6 +29,9 @@ public class EventsController {
 
 	@Autowired
 	private EventService eventService;
+	
+	@Autowired
+	private VenueService venueService;
 
 
 	@ExceptionHandler(EventNotFoundException.class)
@@ -42,6 +53,30 @@ public class EventsController {
 		model.addAttribute("events", eventService.findAll());
 
 		return "events/index";
+	}
+	
+	@GetMapping("/new")
+	public String newEvent(Model model) {
+		if (!model.containsAttribute("event")) {
+			model.addAttribute("event", new Event());
+		}
+		model.addAttribute("venues", venueService.findAll());
+		return "events/new";
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createEvent(@RequestBody @Valid @ModelAttribute Event event, BindingResult errors,
+			Model model, RedirectAttributes redirectAttrs) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			return "events/new";
+		}
+		
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+
+		return "redirect:/events";
 	}
 
 }
